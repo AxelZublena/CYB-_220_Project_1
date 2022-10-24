ff722ca45200e101ec3a4bba238066e5816746f5
AxelZublena <zubl0001@hz.nl>
2022-10-24 15:50:59 -0400

    Add edit functionality + error handling

--- events.py
+++ events.py
@@ -1,13 +1,19 @@
+from rich.console import Console
 from event import Event
 from rich.prompt import Prompt
 
 import random
 import os
+import re
 
 class Events():
 
     def __init__(self):
         self.COLORS = ("red", "blue", "yellow", "green")
+        self.DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+        self.console = Console()
+
         event1 = Event(("Tuesday", 10), 0, "Event 1")
         event2 = Event(("Sunday", 7), 1, "Event 2", "red")
         event3 = Event(("Friday", 8), 2, "Event 3", "yellow")
@@ -24,16 +30,19 @@ class Events():
         input("You will be creating a new event. Press any key to continue....")
         os.system('cls' if os.name == 'nt' else 'clear')
 
-        days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         day = input("Choose a day [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]: ").title()
-        if day not in days:
+        if day not in self.DAYS:
             input(f"{day} is not a valid day. Press any key to continue....")
             return
 
-        time = int(Prompt.ask("Hour (0-23)"))
-        if not 0 <= time < 24:
-            input(f"{time} is not a valid hour. Press any key to continue....")
-            return
+        time = Prompt.ask("Hour (0-23)")
+        try:
+            time = int(time)
+            if not 0 <= time < 24:
+                input(f"{time} is not a valid hour. Press any key to continue....")
+                return
+        except:
+            input(f"{time} is not a valid ID. Press any key to continue....")
 
         title = Prompt.ask("Title")
         event = Event((day, time), len(self.events), title, random.choice(self.COLORS))
@@ -44,6 +53,12 @@ class Events():
         input("You will be deleting an event. Press any key to continue....")
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
+
+            self.console.print("Below are the current events: ")
+            for event in self.events:
+                info = event.get_info()
+                title = info["title"]
+                self.console.print(f" - ID {info['id']} : {title} : {info['date'][0]} at {info['date'][1]}")
 
             event_id = int(input("ID of the event to delete (-1 to abort): "))
             if event_id == -1:
@@ -58,16 +73,49 @@ class Events():
 
     def edit_event(self):
         os.system('cls' if os.name == 'nt' else 'clear')
-        input("You will be deleting an event. Press any key to continue....")
+        input("You will be editing an event. Press any key to continue....")
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
+
+            self.console.print("Below are the current events: ")
+            for event in self.events:
+                info = event.get_info()
+                title = info["title"]
+                self.console.print(f" - ID {info['id']} : {title} : {info['date'][0]} at {info['date'][1]}")
 
             event_id = int(input("ID of the event to edit (-1 to abort): "))
             if event_id == -1:
                 break
-            try:
-                del self.events[event_id]
+
+            value_to_edit = input("What do you want to edit [Title, Hour, Day]? ").lower()
+
+            if value_to_edit == "title":
+                new_title = input("Please provide a new title: ")
+                self.events[event_id].set_title(new_title)
                 break
-            except:
-                input(f"{event_id} is not a valid ID. Press any key to continue....")
+
+            elif value_to_edit == "hour":
+                new_time = int(input("Please provide a new hour (0-23): "))
+                try:
+                    new_time = int(new_time)
+                    if not 0 <= new_time < 24:
+                        input(f"{new_time} is not a valid hour. Press any key to continue....")
+                        return
+                except:
+                    input(f"{new_time} is not a valid ID. Press any key to continue....")
+
+                event = self.events[event_id]
+                info = event.get_info()
+                event.set_date((info["date"][0], new_time))
+                break
+
+            elif value_to_edit == "day":
+                new_day = input("Please provide a new day [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]: ").title()
+                if new_day not in self.DAYS:
+                    input(f"{new_day} is not a valid day. Press any key to continue....")
+                    return
+
+                event = self.events[event_id]
+                info = event.get_info()
+                event.set_date((new_day, info["date"][1]))
                 break
